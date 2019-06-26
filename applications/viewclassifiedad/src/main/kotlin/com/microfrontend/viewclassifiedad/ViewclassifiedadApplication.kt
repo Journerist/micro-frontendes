845f08179c7356a3f7f8ea7e47737f84e3a4a74c
Sebastian Barthel <sebarthel@ebay.com>
2019-06-26 09:18:43 +0200

    Add classified service

---
+++ applications/viewclassifiedad/src/main/kotlin/com/microfrontend/viewclassifiedad/ViewclassifiedadApplication.kt
@@ -0,0 +1,11 @@
+package com.microfrontend.viewclassifiedad
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ViewclassifiedadApplication
+
+fun main(args: Array<String>) {
+	runApplication<ViewclassifiedadApplication>(*args)
+}
